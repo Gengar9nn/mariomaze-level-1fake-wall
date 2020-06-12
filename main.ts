fb079eae868f0fd669c98b80cc65321b7887e1be
Gengar9nn <66795330+Gengar9nn@users.noreply.github.com>
2020-06-12 17:10:33 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,398 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+    //% blockIdentity=images._tile
+    export const tile2 = img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+2 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`
+    //% blockIdentity=images._tile
+    export const tile3 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+a a a a a a a a a a a a a a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 4 4 4 4 4 4 4 4 4 4 a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+a a a 3 a a a a a a a a a a a a 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+a a a a a a a a a a a a a a a a 
+a a a a a a a a a a a a a a a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 5 5 5 5 5 5 5 5 5 5 5 5 a a 
+a a 7 a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a a a a 
+a a 7 a a a a a a a a a a a a a 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+d d e d d d d d d d d e d d d d 
+d d e d d d d d d d d e d d d d 
+d d e d d d d d d d d e d d d d 
+d d e d d d d d d d d e d d d d 
+d d e d d d d d d d d e d d d d 
+d d e d d d d d d d d e d d d d 
+e e e e e e e e e e e e e e e e 
+d d d d e d d d d e d d d d d d 
+d d d d e d d d d e d d d d d d 
+d d d d e d d d d e d d d d d d 
+e e e e e e e e e e e e e e e e 
+d d d d d d e d d d d d d e d d 
+d d d d d d e d d d d d d e d d 
+d d d d d d e d d d d d d e d d 
+d d d d d d e d d d d d d e d d 
+d d d d d d e d d d d d d e d d 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile8 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 
+7 7 7 7 2 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 7 7 2 7 7 7 7 7 2 7 7 7 7 
+7 7 7 7 7 7 2 7 7 7 2 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 2 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile9 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 2 2 2 2 2 2 2 2 2 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 7 2 2 2 2 2 2 2 2 2 2 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile10 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 7 
+7 7 2 7 7 7 7 7 7 7 7 2 7 7 7 7 
+7 7 2 2 7 7 7 7 7 7 7 2 7 7 7 7 
+7 7 7 2 7 7 7 7 7 7 2 2 7 7 7 7 
+7 7 7 2 7 7 7 7 7 7 2 7 7 7 7 7 
+7 7 7 7 2 2 7 7 7 2 2 7 7 7 7 7 
+7 7 7 7 7 2 2 2 2 2 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile11 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile12 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 2 7 7 7 7 
+7 2 7 7 7 7 2 7 7 7 7 2 7 7 7 7 
+7 2 7 7 7 7 2 7 7 7 7 2 7 7 7 7 
+7 2 7 7 7 7 2 7 7 7 7 2 7 7 7 7 
+7 2 7 7 7 7 2 7 7 7 7 2 7 7 7 7 
+7 2 7 7 7 7 2 2 7 7 7 2 7 7 7 7 
+7 2 2 7 7 7 2 2 7 7 2 2 7 7 7 7 
+7 7 2 7 7 2 2 2 2 7 2 7 7 7 7 7 
+7 7 2 2 2 2 7 7 2 2 2 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile13 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 2 2 2 2 2 2 2 2 2 2 2 2 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 2 7 7 7 7 7 7 7 7 
+7 2 2 2 2 2 2 2 2 2 2 2 2 2 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile14 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 2 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 2 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 2 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 2 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 2 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 2 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 2 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 2 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 2 7 7 7 7 7 2 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+    //% blockIdentity=images._tile
+    export const tile15 = img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 2 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile5, function (sprite, location) {
+    info.stopCountdown()
+    tiles.setTilemap(tiles.createTilemap(
+            hex`0a0007000b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b08090a0b0c0d0e0f0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b`,
+            img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15],
+            TileScale.Sixteen
+        ))
+    Ee = 0
+    music.playMelody("C - D - E - F - ", 250)
+    music.playMelody("F - G - A - B C5 ", 250)
+})
+info.onCountdownEnd(function () {
+    game.over(false, effects.melt)
+})
+let Ee = 0
+let Mario = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 2 2 2 2 2 . . . . . 
+. . . 2 2 1 f 1 2 2 2 . . . . . 
+. . . 2 2 f 1 f 2 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 . . . 
+. . . d 1 d d d d d 1 d . . . . 
+. . . d f d d d d f d d . . . . 
+. . . d d f f f f d d d . . . . 
+. . . d d d d d d d d d . . . . 
+1 . . 8 8 8 8 8 8 8 8 8 . . 1 . 
+1 1 . 8 5 8 8 8 8 8 5 8 . 1 1 . 
+. 1 1 8 8 8 8 8 8 8 8 8 1 1 . . 
+. . 1 8 8 8 8 8 8 8 8 8 1 . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 1 8 8 . . . . 
+. . . . 1 . . . . 1 . . . . . . 
+. . . . 1 . . . . 1 . . . . . . 
+. . . . 1 . . . . 1 . . . . . . 
+. . . . 1 . . . . 1 . . . . . . 
+. . . . 1 . . . . 1 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(Mario, 100, 100)
+tiles.setTilemap(tiles.createTilemap(
+            hex`14001400050b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b060a0d0d160d0d0d0d0d0d0d0d0d0d0d0d0d1616160a0d0d160d0d0d0d0d0d0d0d0d0d160d161615160a0d0d160d0d0d0d0d160d0d0d0d160d161616160a0d161616160d161616161616161616161616160a0d0d160d0d0d161616161616161616161616160a0d0d160d0d0d0d0d16161616161616161616160a0d0d160d0d0d0d0d16161616161616161616160a0d0d160d0d0d0d0d16161616161616161616160a0d0d0d0d160d0d0d0d0d1616161616161616160a0d0d0d0d160d0d0d0d0d161616160d0d0d16160a0d0d0d0d160d0d0d0d0d161616160d0d0d0d160a0d140d0d160d0d0d0d0d1616161616160d16160a0d0d0d0d160d0d161616161616160d0d0d160c0a0d0d0d0d160d0d161616161616160d0d0d0d0c0a0d0d0d0d160d0d161616161616160d160d160c0a0d0d0d160d0d161616160d0d0d0d0d0d160d0c0a0d16161616160d160d0d0d0d0d0d0d160d160c0a0d0d0d160d0d0d16160d0d0d0d0d0d0d0d0d0c0709090916090909090909090909090909090908`,
+            img`
+. . . . . . . . . . . . . . . . . . . . 
+. . . 2 . . . . . . . . . . . . . 2 2 2 
+. . . 2 . . . . . . . . . . 2 . 2 2 . 2 
+. . . 2 . . . . . 2 . . . . 2 . 2 2 . 2 
+. . 2 2 2 2 . 2 2 2 2 2 2 2 2 2 2 2 . 2 
+. . . 2 . . . 2 2 2 2 2 2 2 2 2 2 2 . 2 
+. . . 2 . . . . . 2 2 2 2 2 2 2 2 2 . 2 
+. . . 2 . . . . . 2 2 2 2 2 . . . . . 2 
+. . . 2 . . . . . 2 2 2 2 2 . . . . . 2 
+. . . . . 2 . . . . . 2 2 2 . 2 2 2 2 2 
+. . . . . 2 . . . . . 2 2 2 . . . . 2 2 
+. . . . . 2 . . . . . . . . . . . . . 2 
+. . . . . 2 . . . . . . . . . 2 2 . 2 2 
+. . . . . 2 . . 2 2 2 2 2 2 2 . . . 2 . 
+. . . . . 2 . . 2 2 2 2 2 2 2 . . . . . 
+. . . . . 2 . . 2 2 2 2 2 2 2 . 2 . 2 . 
+. . . . 2 . . 2 2 2 2 . . . . . . 2 . . 
+. . 2 2 2 2 2 . 2 . . . . . . . 2 . 2 . 
+. . . . 2 . . . 2 2 . . . . . . . . . . 
+. . . . 2 . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,sprites.dungeon.collectibleInsignia,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.collectibleBlueCrystal,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.chestOpen,sprites.dungeon.chestClosed,myTiles.tile4,myTiles.tile5,sprites.builtin.brick,sprites.builtin.crowd7,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15],
+            TileScale.Sixteen
+        ))
+tiles.placeOnRandomTile(Mario, myTiles.tile4)
+scene.cameraFollowSprite(Mario)
+info.startCountdown(30)
+game.showLongText("Help Mario to reach the yellow flag!", DialogLayout.Full)
+Ee = 1
+forever(function () {
+    if (Ee == 1) {
+        music.playMelody("C D E F G F E D ", 120)
+        music.playMelody("C - C D C - C D ", 120)
+    }
+})
